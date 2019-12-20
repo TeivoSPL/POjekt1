cb4486db58e637b3f8b8066b9f90742a176f4d85
Witold Serwatka <witold.serwatka@pm.me>
2019-12-20 18:33:51 +0100

    Started working on main

--- src/main/projekt1/ecosystem/World.java
+++ src/main/projekt1/ecosystem/World.java
@@ -1,10 +1,28 @@
 package main.projekt1.ecosystem;
 
+import java.util.Scanner;
+
 public class World {
 
 
-    public static void main(String[] args) {
-        System.out.println(">Do you want to read variables from an external file? (y/n)");
+    public static void main(String[] args) { //dodaj ioexception
+        Scanner input = new Scanner(System.in);
 
+        String externalFile;
+        do {
+            System.out.println("Do you want to read variables from an external file? (y/n)");
+            externalFile = input.next("> ");
+
+            switch (externalFile) {
+                case "y":
+                    JSONObject
+                    break;
+                case "n":
+
+                    break;
+                default:
+                    System.out.println("Could not understand input.");
+            }
+        }while(!(externalFile.equals("y") || externalFile.equals("n")));
     }
 }
